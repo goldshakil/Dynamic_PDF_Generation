a06c3c23b3d9d17e3e934c412647c29d2d8d8dcf
Zuhaib Shakeel (Dahab) <48248272+goldshakil@users.noreply.github.com>
2021-03-30 14:57:28 +0900

    Add files via upload

---
+++ report_generator.py
@@ -0,0 +1,36 @@
+from fpdf import FPDF
+from datetime import datetime, timedelta
+import os
+
+WIDTH = 210
+HEIGHT = 297
+
+
+def create_report(patient_name, interp_date, study_date, patient_code, interp_dr, sex, dob, height, weight, bmi, diagnosis):
+    pdf = FPDF()
+    pdf.set_left_margin(margin=25)
+    pdf.set_right_margin(margin=25)
+
+    # First Page
+    pdf.add_page()
+
+    pdf.image('resources/logo.png',
+              x=(WIDTH-150)/2, y=2, w=150, type='PNG')
+    pdf.ln(20)
+
+    pdf.set_font(family='Arial', style='B', size=10)
+    pdf.multi_cell(
+        w=0, h=4, txt='1060 S. Main St., Suite 301A\nSt. George, UT 84770\n(855) 276-3263', align='C')
+
+    pdf.ln(5)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=0, h=4, txt='HOME SLEEP TEST ANALYSIS REPORT', align='L')
+
+    # Second Page
+    # Output
+    pdf.output('sibel-report.pdf', 'F')
+
+
+if __name__ == '__main__':
+    create_report(patient_name='Dahab Shakeel', interp_date='9/3/2019', study_date='8/26/2019', patient_code='TS082619',
+                  interp_dr='Chandra Matadeen-Ali, MD', sex='M', dob='10/23/1998', height=186, weight=75, bmi='', diagnosis='Severe Obstructive Sleep Apnea (G47.33)')
