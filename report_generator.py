f6d5336710a067242b0033249af9b2edfa51fbbc
Zuhaib Shakeel (Dahab) <48248272+goldshakil@users.noreply.github.com>
2021-03-30 17:21:17 +0900

    added recommendation class

--- report_generator.py
+++ report_generator.py
@@ -6,7 +6,16 @@ WIDTH = 210
 HEIGHT = 297
 
 
-def create_report(patient_name, interp_date, study_date, patient_code, interp_dr, sex, dob, height, weight, bmi, recording_time, monitoring_time, ahi, odi, mean_spo2, min_spo2, mean_heart_rate, diagnosis):
+class recommendation:
+    def __init__(self, recommendation_data):
+        self.recommendation_data = recommendation_data
+        self.sub_list = []
+
+    def add_sub_list(self, sub_list_data):
+        self.sub_list.append(sub_list_data)
+
+
+def create_report(patient_name, interp_date, study_date, patient_code, interp_dr, sex, dob, height, weight, bmi, recording_time, monitoring_time, ahi, odi, mean_spo2, min_spo2, mean_heart_rate, diagnosis, recommendations):
     pdf = FPDF()
     pdf.set_left_margin(margin=25)
     pdf.set_right_margin(margin=25)
@@ -86,6 +95,22 @@ def create_report(patient_name, interp_date, study_date, patient_code, interp_dr
     pdf.set_font(family='Arial', style='B', size=10)
     pdf.cell(w=(WIDTH-2*25-18), h=4,
              txt=f'{diagnosis}', align='L', ln=1)
+    pdf.ln(5)
+
+    pdf.set_font(family='Arial', style='', size=9)
+    pdf.cell(w=0, h=4, txt='Recommendations: ', align='L')
+    pdf.ln(4)
+
+    for i in recommendations:
+        pdf.cell(w=8, h=4,
+                 txt='  -', align='L')
+        pdf.multi_cell(w=(WIDTH-2*25-18), h=4,
+                       txt=i.recommendation_data)
+        for j in i.sub_list:
+            pdf.cell(w=8, h=4, align='L')
+            pdf.cell(w=8, h=4, txt='  -', align='L')
+            pdf.multi_cell(w=(WIDTH-2*25-18), h=4, txt=j)
+        pdf.ln(2)
 
     # Second Page
     # Output
@@ -93,5 +118,19 @@ def create_report(patient_name, interp_date, study_date, patient_code, interp_dr
 
 
 if __name__ == '__main__':
+
+    recommendation1 = recommendation('Consider nasal continuous positive airway pressure (CPAP) as the initial treatment choice for obstructive sleep apnea. If the patient chooses CPAP therapy, a nocturnal PSG with CPAP titration is recommended. As an alternative, an Auto PAP with pressure range 5-20 cmH20 with download is an option. Consider follow up overnight pulse oximetry test on Auto PAP therapy after OSA is controlled. Consider PAP interface (mask) fitted for patient comfort, heated humidification & PAP compliance monitoring (1month, 3 months & 12 months after PAP initiation).')
+
+    recommendation2 = recommendation(
+        'Positive airway pressure therapy (PAP) is the first line of treatment for patients with OSA. Alternative treatment for OSA in patients who cannot tolerate and have failed or refused CPAP therapy includes:')
+    recommendation2.add_sub_list(
+        'The patient may benefit from the use of a nocturnal mandibular repositioning appliance. If that line of therapy is to be pursued, the patient should be evaluated by a dentist trained in the treatment of sleep related breathing disorders.')
+    recommendation2.add_sub_list(
+        'An ENT consultation which may be useful to look for specific causes of obstruction and possible treatment options.')
+
+    recommendation_list = []
+    recommendation_list.append(recommendation1)
+    recommendation_list.append(recommendation2)
+
     create_report(patient_name='Dahab Shakeel', interp_date='9/3/2019', study_date='8/26/2019', patient_code='TS082619',
-                  interp_dr='Chandra Matadeen-Ali, MD', sex='M', dob='10/23/1998', height=184, weight=72, bmi=21.3, recording_time=406.3, monitoring_time=400.8, ahi=35.2, odi=42.3, mean_spo2=92, min_spo2=74, mean_heart_rate=77.5, diagnosis='Severe Obstructive Sleep Apnea (G47.33)')
+                  interp_dr='Chandra Matadeen-Ali, MD', sex='M', dob='10/23/1998', height=184, weight=72, bmi=21.3, recording_time=406.3, monitoring_time=400.8, ahi=35.2, odi=42.3, mean_spo2=92, min_spo2=74, mean_heart_rate=77.5, diagnosis='Severe Obstructive Sleep Apnea (G47.33)', recommendations=recommendation_list)
