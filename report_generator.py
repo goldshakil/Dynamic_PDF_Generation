b8962d05017435d1da209826b6dc4ce3a0cabce3
Zuhaib Shakeel (Dahab) <48248272+goldshakil@users.noreply.github.com>
2021-03-31 14:36:39 +0900

    added table 2 and 3

--- report_generator.py
+++ report_generator.py
@@ -18,7 +18,7 @@ class recommendation:
         self.sub_list.append(sub_list_data)
 
 
-def create_report(patient_name, interp_date, study_date, patient_code, interp_dr, sex, dob, height, weight, bmi, recording_time, monitoring_time, ahi, odi, mean_spo2, min_spo2, mean_heart_rate, diagnosis, recommendations, age):
+def create_report(patient_name, interp_date, study_date, patient_code, interp_dr, sex, dob, height, weight, bmi, recording_time, monitoring_time, ahi, odi, mean_spo2, min_spo2, mean_heart_rate, diagnosis, recommendations, age, lights_off_time, lights_on_time, bed_time):
     pdf = FPDF()
     pdf.set_left_margin(margin=25)
     pdf.set_right_margin(margin=25)
@@ -186,6 +186,50 @@ def create_report(patient_name, interp_date, study_date, patient_code, interp_dr
     pdf.cell(w=20, h=5, txt='BMI', align='L', border=1)
     pdf.set_font(family='Arial', style='', size=10)
     pdf.cell(w=60, h=5, txt=f'{bmi}', align='L', border=1, ln=1)
+    pdf.ln(5)
+
+    pdf.set_font(family='Arial', style='B', size=10)  # Table-2 Header
+    pdf.cell(w=REMAINING_WIDTH, h=7, txt='Times and Durations',
+             align='L', border=1, fill=True, ln=1)
+
+    pdf.set_font(family='Arial', style='B', size=10)  # Row 1
+    pdf.cell(w=40, h=5, txt='Lights off clock time', align='L', border=1)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=40, h=5, txt=f'{lights_off_time}', align='L', border=1)
+    pdf.set_font(family='Arial', style='B', size=10)
+    pdf.cell(w=40, h=5, txt='Total Time Recording', align='L', border=1)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=40, h=5, txt=f'{recording_time} minutes',
+             align='L', border=1, ln=1)
+
+    pdf.set_font(family='Arial', style='B', size=10)  # Row 2
+    pdf.cell(w=40, h=5, txt='Lights on clock time', align='L', border=1)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=40, h=5, txt=f'{lights_on_time}', align='L', border=1)
+    pdf.set_font(family='Arial', style='B', size=10)
+    pdf.cell(w=40, h=5, txt='Time In Bed', align='L', border=1)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=40, h=5, txt=f'{bed_time} minutes',
+             align='L', border=1, ln=1)
+
+    pdf.set_font(family='Arial', style='B', size=10)  # Row 3
+    pdf.cell(w=40, h=5, txt='', align='L', border=1)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=40, h=5, txt='', align='L', border=1)
+    pdf.set_font(family='Arial', style='B', size=10)
+    pdf.cell(w=40, h=5, txt='Monitoring Time', align='L', border=1)
+    pdf.set_font(family='Arial', style='', size=10)
+    pdf.cell(w=40, h=5, txt=f'{monitoring_time} minutes',
+             align='L', border=1, ln=1)
+    pdf.ln(5)
+
+    pdf.set_font(family='Arial', style='B', size=10)  # Table-3 Header
+    pdf.cell(w=REMAINING_WIDTH, h=7, txt='Device and Sensor Details',
+             align='L', border=1, fill=True, ln=1)
+
+    pdf.set_font(family='Arial', style='', size=10)  # Row 1
+    pdf.multi_cell(
+        w=REMAINING_WIDTH, h=4, txt='The study was recorded on a Philips Respironics Alice NightOne device using 1 RIP effort belt and a pressure based flow sensor. The heart rate is derived from the oximeter sensor and the snore signal is derived from the pressure sensor. The device also records body position and uses it to determine the monitoring time(sleep/wake periods).', align='', border=1)
 
     # Third Page
 
@@ -209,4 +253,4 @@ if __name__ == '__main__':
     recommendation_list.append(recommendation2)
 
     create_report(patient_name='Dahab Shakeel', interp_date='9/3/2019', study_date='8/26/2019', patient_code='TS082619',
-                  interp_dr='Chandra Matadeen-Ali, MD', sex='M', dob='10/23/1998', height=184, weight=160, bmi=21.3, recording_time=406.3, monitoring_time=400.8, ahi=35.2, odi=42.3, mean_spo2=92, min_spo2=74, mean_heart_rate=77.5, diagnosis='Severe Obstructive Sleep Apnea (G47.33)', recommendations=recommendation_list, age=22)
+                  interp_dr='Chandra Matadeen-Ali, MD', sex='M', dob='10/23/1998', height=184, weight=160, bmi=21.3, recording_time=406.3, monitoring_time=400.8, ahi=35.2, odi=42.3, mean_spo2=92, min_spo2=74, mean_heart_rate=77.5, diagnosis='Severe Obstructive Sleep Apnea (G47.33)', recommendations=recommendation_list, age=22, lights_off_time="11:00:25 PM", lights_on_time="5:46:43 AM", bed_time=406.3)
